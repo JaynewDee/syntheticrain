2b7e78a564648480b559081efa586c4f7f3e4c7b
NuminousBlue <jdiehl2236@gmail.com>
2022-01-07 15:34:37 -0600

    initialized essential dependencies app base and components

---
+++ src/components/Atom.js
@@ -0,0 +1,9 @@
+import logo from '../logo.svg';
+
+export default function Atom(){ 
+     return (
+          <header className="App-header">
+               <img src={logo} className="App-logo" alt="logo" />
+          </header>
+     )
+}
